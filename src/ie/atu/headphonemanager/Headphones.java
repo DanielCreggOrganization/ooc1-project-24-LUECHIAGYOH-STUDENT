6567496c0da417b2fa159dfd994a3f419df39f3f
LUECHIAGYOH-STUDENT <145437151+LUECHIAGYOH-STUDENT@users.noreply.github.com>
2024-11-21 17:05:19 +0000

    headphone

--- src/ie/atu/headphonemanager/Headphones.java
+++ src/ie/atu/headphonemanager/Headphones.java
@@ -8,53 +8,44 @@ public class Headphones {
     private float price;
     private boolean isSold;
 
-    public Headphones(int serialNumber, String brand, float price, boolean isSold){
+    public Headphones(int serialNumber, String brand, float price, boolean isSold) {
         this.serialNumber = serialNumber;
         this.brand = brand;
         this.price = price;
         this.isSold = isSold;
     }
 
-
-
-
     // getter and Setter Methods
-    public int getSerialNumber(){
+    public int getSerialNumber() {
         return this.serialNumber;
     }
 
-    public String getbrand(){
+    public String getbrand() {
         return this.brand;
     }
 
-    public float getprice(){
+    public float getprice() {
         return this.price;
     }
 
-    public boolean getisSold(){
+    public boolean getisSold() {
         return this.isSold;
     }
 
-    public void getbrand(String brand){
+    public void getbrand(String brand) {
         this.brand = brand;
     }
 
-    public void getserialNumber(int serialNumber){
+    public void getserialNumber(int serialNumber) {
         this.serialNumber = serialNumber;
     }
 
-    public void getprice(float price){
+    public void getprice(float price) {
         this.price = price;
     }
 
-    public void getisSold(boolean isSold){
+    public void getisSold(boolean isSold) {
         this.isSold = isSold;
     }
-
-    
-
-    
-
-
 
 }
