a2a85b6b84327dda475b504e55f8bbc7f2bbee30
LUECHIAGYOH-STUDENT <145437151+LUECHIAGYOH-STUDENT@users.noreply.github.com>
2024-11-21 16:11:36 +0000

    headphone

--- src/ie/atu/headphonemanager/Headphones.java
+++ src/ie/atu/headphonemanager/Headphones.java
@@ -2,4 +2,33 @@ package ie.atu.headphonemanager;
 
 public class Headphones {
 
+    // Instance Variables
+    private int serialNumber; // Unique10
+    private String brand;
+    private float price;
+    private boolean isSold;
+
+    // getter and Setter Methods
+    public int getSerialNumber(){
+        return this.serialNumber;
+    }
+
+    public void getserialNumber(int serialNumber){
+        this.serialNumber = serialNumber;
+    }
+
+    public float getprice(){
+        return this.price;
+    }
+
+    public void getprice(int price){
+        this.price = price;
+    }
+
+    public boolean getisSold(){
+        return this.isSold;
+    }
+
+
+
 }
